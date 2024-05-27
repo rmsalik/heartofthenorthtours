bb20745af5d213894ca06fee5101d485d9837a21
Arslangrewal <134731188+Arslangrewal@users.noreply.github.com>
2024-05-27 15:56:49 +0500

    Solve the tours error

--- app/Models/Tours.php
+++ app/Models/Tours.php
@@ -2,13 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Gallery;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Tours extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
-    
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::deleting(function ($tour) {
+            // Delete related gallery records
+            $tour->gallery()->delete();
+        });
+    }
+
+    public function gallery()
+    {
+        return $this->hasMany(Gallery::class, 'tour_id', 'id');
+    }
+
 }
